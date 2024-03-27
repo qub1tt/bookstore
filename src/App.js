da826690fd6ee1a268d85a57be69943ac129c002
qub1t <duyhoangnguyen1711@gmail.com>
2024-03-27 12:04:33 +0700

    add Order

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/Homepage/HomePage";
 import BookDetailsPage from "./pages/BookDetails";
 import Cart from "./pages/Cart/Cart";
 import Profile from "./pages/Profile/Profile";
+import Order  from "./pages/Profile/Order";
 const App = () => {
   return (
     <div>
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/book-details/:id" element={<BookDetailsPage />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/order" element={<Order />} />
       </Routes>
     </div>
   );
