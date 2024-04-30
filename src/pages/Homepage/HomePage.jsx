f70060a8286062b8aaa0a17b50d9c5c1e66c6570
qub1t <duyhoangnguyen1711@gmail.com>
2024-04-30 21:00:07 +0700

    fix

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -5,7 +5,7 @@ import Content1 from "../../components/content1/content1";
 import Content0 from "../../components/content0/content0";
 import Content2 from "../../components/content2/content2";
 import Footer from "../../components/footer/footer";
-import Book_Carousel from "../../components/book_carousel/book_carousel"
+import Book_Carousel from "../../components/book_carousel/book_carousel";
 import "./HomePage.css";
 
 const HomePage = () => {
@@ -17,8 +17,11 @@ const HomePage = () => {
       <div className="navbar">
         <Abovenav />
       </div>
+    
       <div className="content">
-        <div className="book_carousel"><Book_Carousel /></div>
+        <div className="book_carousel">
+          <Book_Carousel />
+        </div>
         <Content1 />
         <Content2 />
         <Content0 />
