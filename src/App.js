457f93c46dad835384a61e114f10a8729b906129
qub1t <duyhoangnguyen1711@gmail.com>
2024-05-07 20:17:33 +0700

    add not found and loading page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useParams } from "react-router-dom";
 import HomePage from "./pages/Homepage/HomePage";
 import BooksDetailContainer from "./containers/book.detail.container";
 import Cart from "./pages/Cart/Cart";
 import Profile from "./pages/Profile/Profile";
 import Order from "./pages/Profile/Order";
-
+import NotFound from "./components/404/NotFound";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/book/:id" element={<BooksDetailContainer/>} />
+        <Route path="/book/:id" element={<BooksDetailContainer />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/order" element={<Order />} />
+        <Route path="/loading" element={<NotFound />} />
       </Routes>
     </Router>
   );
