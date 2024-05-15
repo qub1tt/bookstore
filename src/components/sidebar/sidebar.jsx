21301c8e908aee4090e9b8ac42e92e969f75ebbb
qub1t <duyhoangnguyen1711@gmail.com>
2024-05-15 15:25:50 +0700

    fix logout

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -81,7 +81,10 @@ class Sidebar extends Component {
                         style={{ color: "#737373" }}
                       />
                     </span>
-                    <a href="/category/662a796042e68c567e688052" class="sidebar_title">
+                    <a
+                      href="/category/662a796042e68c567e688052"
+                      class="sidebar_title"
+                    >
                       Tâm lý
                     </a>
                   </li>
@@ -93,7 +96,10 @@ class Sidebar extends Component {
                         style={{ color: "#737373" }}
                       />
                     </span>
-                    <a href="/category/662a796642e68c567e688053" class="sidebar_title">
+                    <a
+                      href="/category/662a796642e68c567e688053"
+                      class="sidebar_title"
+                    >
                       Kinh tế
                     </a>
                   </li>
@@ -105,7 +111,10 @@ class Sidebar extends Component {
                         style={{ color: "#737373" }}
                       />
                     </span>
-                    <a href="/category/662a796a42e68c567e688054" class="sidebar_title">
+                    <a
+                      href="/category/662a796a42e68c567e688054"
+                      class="sidebar_title"
+                    >
                       Văn học
                     </a>
                   </li>
@@ -117,7 +126,10 @@ class Sidebar extends Component {
                         style={{ color: "#737373" }}
                       />
                     </span>
-                    <a href="/category/662a796d42e68c567e688055" class="sidebar_title">
+                    <a
+                      href="/category/662a796d42e68c567e688055"
+                      class="sidebar_title"
+                    >
                       Truyện tranh
                     </a>
                   </li>
@@ -129,7 +141,10 @@ class Sidebar extends Component {
                         style={{ color: "#737373" }}
                       />
                     </span>
-                    <a href="/category/662a7a5142e68c567e688057" class="sidebar_title">
+                    <a
+                      href="/category/662a7a5142e68c567e688057"
+                      class="sidebar_title"
+                    >
                       Khoa học
                     </a>
                   </li>
@@ -141,7 +156,10 @@ class Sidebar extends Component {
                         style={{ color: "#737373" }}
                       />
                     </span>
-                    <a href="/category/662a7a5842e68c567e688058" class="sidebar_title">
+                    <a
+                      href="/category/662a7a5842e68c567e688058"
+                      class="sidebar_title"
+                    >
                       Sách giáo khoa
                     </a>
                   </li>
@@ -167,7 +185,10 @@ class Sidebar extends Component {
                         style={{ color: "#737373" }}
                       />
                     </span>
-                    <a href="/profile/${this.state.email}" class="sidebar_title">
+                    <a
+                      href="/profile/${this.state.email}"
+                      class="sidebar_title"
+                    >
                       Hồ sơ
                     </a>
                   </li>
@@ -204,5 +225,3 @@ class Sidebar extends Component {
 }
 
 export default Sidebar;
-
-
