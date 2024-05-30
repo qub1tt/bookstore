ee9b1c4e32cd2e212459595c8928bbddab0914fb
Le Ca <22520140@gm.uit.edu.vn>
2024-05-30 23:13:52 +0700

    fix something'

--- src/components/login/loginDetails.jsx
+++ src/components/login/loginDetails.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import React, { useState } from "react";
+import "./login.css"
 
 export default function ContentLoginRegister({
   setEmailLogin,
@@ -33,223 +34,80 @@ export default function ContentLoginRegister({
 
   let xhtmlLogin = "";
   let xhtmlRegister = "";
-
   if (Login) {
     xhtmlLogin = (
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 mb-5">
-            Login to your account
-          </h2>
-          <div className="noti">{notificationLogin}</div>
+    <div className="login-page">
+        <div className="login-form">
+            <h2 className="login-title">
+                Login to your account
+            </h2>
+            <div className="noti">{notificationLogin}</div>
+
+            <div className="login-input">
+                <input type="email" onChange={(e) => { setEmailLogin(e.target.value);}} className="" placeholder="Email" />
+            </div>
+
+            <div className="login-input">  
+                <input type="password" onChange={(e) => { setPasswordlogin(e.target.value); }} className="" placeholder="Password" /> 
+            </div>
+
+            <button type="submit" className="login-button" onClick={() => loginSubmit()} > Sign in </button>
+
+            <div className="login-link"> <Link to="/forgotpass/" className="" > Forgot Password? </Link> </div>
+
+            <div className="flex justify-between items-center">
+                <p>Don't have an account?</p>
+                <Link onClick={handleRegister} className="login-link text-indigo-600 text-base">Sign up</Link>
+            </div>
         </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <div className="space-y-6">
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  type="email"
-                  onChange={(e) => {
-                    setEmailLogin(e.target.value);
-                  }}
-                  className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Password
-                </label>
-                <div className="text-sm">
-                  <Link
-                    to="/forgotpass/"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot Password?
-                  </Link>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  type="password"
-                  onChange={(e) => {
-                    setPasswordlogin(e.target.value);
-                  }}
-                  className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                onClick={() => loginSubmit()}
-              >
-                Sign in
-              </button>
-              <div className="flex justify-between my-4">
-                <p>Do you have an account?</p>
-                <button onClick={handleRegister} className="font-semibold text-indigo-600 hover:text-indigo-500">Đăng Ký</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    </div>
     );
   }
 
   if (Register) {
     xhtmlRegister = (
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+      <div className="login-page">
+        <div className="login-form">
+          <h2 className="login-title">
             Sign Up
           </h2>
           <div className="noti">{notificationRegister}</div>
-        </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <div className="space-y-6">
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  type="email"
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
-                  className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+            <div className="login-input">
+                <input type="email" onChange={(e) => { setEmail(e.target.value); }} className="" placeholder="Email" />
             </div>
-
-            <div>
-              <label className="block text-sm font-medium leading-6 text-gray-900">
-                First Name
-              </label>
-
-              <div className="mt-2">
-                <input
-                  type="text"
-                  onChange={(e) => {
-                    setFirstname(e.target.value);
-                  }}
-                  className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+            <div className="login-input">
+                <input type="text" onChange={(e) => { setFirstname(e.target.value); }} className="" placeholder="First Name"/>
             </div>
-
-            <div>
-              <label className="block text-sm font-medium leading-6 text-gray-900">
-                Last Name
-              </label>
-
-              <div className="mt-2">
-                <input
-                  type="text"
-                  onChange={(e) => {
-                    setLastname(e.target.value);
-                  }}
-                  className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+            <div className="login-input">
+                <input type="text" onChange={(e) => { setLastname(e.target.value); }} className="" placeholder="Last Name"/>
             </div>
-
-            <div>
-              <label className="block text-sm font-medium leading-6 text-gray-900">
-                Address
-              </label>
-
-              <div className="mt-2">
-                <input
-                  type="text"
-                  onChange={(e) => {
-                    setAddress(e.target.value);
-                  }}
-                  className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+            <div className="login-input">
+                <input type="text" onChange={(e) => { setAddress(e.target.value); }} className="" placeholder="Address"/>
             </div>
-
-            <div>
-              <label className="block text-sm font-medium leading-6 text-gray-900">
-                Phone Number
-              </label>
-
-              <div className="mt-2">
-                <input
-                  type="number"
-                  onChange={(e) => {
-                    setPhone(e.target.value);
-                  }}
-                  className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+            <div className="login-input">
+                <input type="number" onChange={(e) => { setPhone(e.target.value); }} className="" placeholder="Phone Number"/>
+            </div>    
+            <div className="login-input">
+                <input type="password" onChange={(e) => setPassword(e.target.value)} className="" placeholder="Password"/>
             </div>
-
-            <div>
-              <label className="block text-sm font-medium leading-6 text-gray-900">
-                Password
-              </label>
-
-              <div className="mt-2">
-                <input
-                  type="password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+            <div className="login-input">
+                <input type="password" onChange={(e) => { setConfirm(e.target.value); }} className="" placeholder="Confirm Password"/>
             </div>
-
-            <div>
-              <label className="block text-sm font-medium leading-6 text-gray-900">
-                Confirm Password
-              </label>
-
-              <div className="mt-2">
-                <input
-                  type="password"
-                  onChange={(e) => {
-                    setConfirm(e.target.value);
-                  }}
-                  className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
+            <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="login-button"
                 onClick={() => registerSubmit()}
-              >
+            >
                 Sign up
-              </button>
+            </button>
+            <div className="flex justify-between items-center">
+                <p>Already have an account?</p>
+                <Link onClick={handleLogin} className="login-link text-indigo-600 text-base">Sign in</Link>
             </div>
-          </div>
         </div>
       </div>
     );
   }
-
   return (
     <section className="homePage">
       <div className="login-register">
